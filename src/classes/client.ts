279ccc052d7c16cd4f4aafe394c2b7b56fe56c25
The-Glit-ch <58119800+The-Glit-ch@users.noreply.github.com>
2023-05-26 15:32:00 -0400

    Initial project commit

---
+++ src/classes/client.ts
@@ -0,0 +1,42 @@
+// Imports
+import { Pool } from 'pg';
+
+// Docstring
+
+// Enums
+
+// Interfaces
+
+// Classes
+export class Client{
+	// Public Variables
+
+	// Private Variables
+
+	// Constructor
+	constructor () {
+		
+	}
+
+	// Public Static Methods
+
+	// Public Inherited Methods
+
+	// Private Static Methods
+
+	// Private Inherited Methods
+}
+
+// Constants
+
+// Public Variables
+
+// Private Variables
+
+// _init()
+
+// Public Methods
+
+// Private Methods
+
+// Run
---
+++ src/index.ts
@@ -0,0 +1,40 @@
+// Imports
+
+// Docstring
+/**
+ * Just Another Postgres Library @ Index.ts
+ * Library index file
+ */
+
+// Enums
+
+// Interfaces
+
+// Classes
+	// Public Variables
+
+	// Private Variables
+
+	// Constructor
+
+	// Public Static Methods
+
+	// Public Inherited Methods
+
+	// Private Static Methods
+
+	// Private Inherited Methods
+
+// Constants
+
+// Public Variables
+
+// Private Variables
+
+// _init()
+
+// Public Methods
+
+// Private Methods
+
+// Run
